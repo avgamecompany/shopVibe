ed7c5aa17715dd5b8829e1f0d27df7d4fdde420e
avgamecompany <originalavgamecompany@gmail.com>
2024-11-09 09:14:01 +0200

    Add files via upload

---
+++ oculus.js
@@ -0,0 +1,9 @@
+function prime(x) {
+
+    var d=document.createElement("div")
+    d.innerText =x
+ document.getElementById("x").appendChild(d)
+}
+
+
+prime("cost $279.99")
---
+++ play.js
@@ -0,0 +1,9 @@
+function prime(x) {
+
+    var d=document.createElement("div")
+    d.innerText =x
+ document.getElementById("x").appendChild(d)
+}
+
+
+prime("what would you like to buy?")
